37765bf2f9bdea95f8a64993e87b8653abbb8d18
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-06 10:45:04 -0800

    prep for heroku

--- server.js
+++ server.js
@@ -37,4 +37,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/app/build/index.html"));
+});
+
 app.listen(port, () => console.log(`Server started on port: ${port}`));
